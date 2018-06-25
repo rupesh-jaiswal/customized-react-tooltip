cb5e0633aee2a16bf2c0b907aa0ab81ff7948eb7
Rupesh Jaiswal <RupeshJ@citiustech.com>
2018-06-25 20:34:26 +0530

    added color dynamically

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import { Tooltip, OverlayTrigger, Button } from 'react-bootstrap';
 import ReactTooltip from 'react-tooltip'
 //import 'grommet/scss/vanilla/index';
 class App extends Component {
-  onHover=() => {
-    this.setState({hover:!this.state.hover});
+  onHover=(el) => {
+    console.log(el);
+    this.setState({hover:!this.state.hover , el});
   }
   state={
-    hover:false
+    hover:false,
+    el:'dummy'
   }
   render() {
     //const tooltip=<Tooltip message="this is a hover message" />;
@@ -21,34 +23,54 @@ class App extends Component {
         <div style={{ padding:"10px"}}>Hellow World</div>
       </Tooltip>
     );
+    let colorClass='critical';
+    if(this.state.el=='custom1') {
+      colorClass='ok';
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <div>
-          <h1 className="App-title" data-tip='' data-for='test' onMouseOver={this.onHover} onMouseOut={this.onHover} style={{cursor:'pointer'}}>Welcome to React</h1>
+          <h1 className="App-title" data-tip='' data-for='custom1' onMouseOver={this.onHover.bind(this,'custom1')} onMouseOut={this.onHover.bind(this,'dummy')} style={{cursor:'pointer'}}>Welcome to React</h1>
+          <h1 className="App-title1" data-tip='' data-for='custom2' onMouseOver={this.onHover.bind(this,'custom2')} onMouseOut={this.onHover.bind(this, 'dummy')} style={{cursor:'pointer'}}>Welcome to Angular</h1>
           { this.state.hover && <Tooltip placement="right" className="in" id="tooltip-right">Hellow World</Tooltip> }
           { this.state.error && <Tip target='App-title'>
               Available actions
               </Tip>}
           </div>
         </header>
-        <ReactTooltip  id='test' >
-          <p>This is a global react component tooltip</p>
-          <p>You can put every thing here</p>
-          <ul>
-            <li>Word</li>
-            <li>Chart</li>
-            <li>Else</li>
-          </ul>
+
+        <div className={colorClass}>
+          <ReactTooltip  className="custom1" place="right" id="custom1" >
+            <p>This is a global react component tooltip</p>
+            <p>You can put every thing here</p>
+            <ul>
+              <li>Word</li>
+              <li>Chart</li>
+              <li>Else</li>
+            </ul>
           </ReactTooltip>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <ReactTooltip  id="custom2" >
+            <p>This is a global react component tooltip</p>
+            <p>You can put every thing here</p>
+            <ul>
+              <li>Word</li>
+              <li>Chart</li>
+              <li>Else</li>
+            </ul>
+          </ReactTooltip>
+        </div>
         <Tooltip placement="right" className="in" id="tooltip-right"><div style={{ padding:"10px"}}>Hellow World</div></Tooltip>
         <OverlayTrigger placement="bottom" overlay={tooltip}>
           <Button bsStyle="default">Holy guacamole!</Button>
         </OverlayTrigger>
+
+        <div
+          className="col-xs-7 col-sm-4 col-md-4 col-lg-4"
+        >
+          Hello World
+        </div>
       </div>
     );
   }
